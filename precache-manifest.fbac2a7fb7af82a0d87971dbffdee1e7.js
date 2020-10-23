73fb3353363c658dfcb99cc12948221168835608
hugo-wb <hugowong71@gmail.com>
2020-10-23 16:37:31 +0100

    Updates

---
+++ precache-manifest.fbac2a7fb7af82a0d87971dbffdee1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c69b373ffa2d64ac3fe4a6ad05575ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48ae3fbae1e0d0575f86",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e7b81daa5f8190f998ee",
+    "url": "/static/js/2.678966aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.678966aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ae3fbae1e0d0575f86",
+    "url": "/static/js/main.fc68ac8e.chunk.js"
+  },
+  {
+    "revision": "7b2a721b80c7c344ce76",
+    "url": "/static/js/runtime-main.fb51407d.js"
+  }
+]);

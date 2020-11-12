a9df9e1701346663b963ec3231cfcaee4b802e45
hugo-wb <hugowong71@gmail.com>
2020-11-12 15:59:26 +0000

    about me

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,13 +4,8 @@ export default class About extends Component {
     render() {
         return (
             <div>
-                <h1 className = "title">Title</h1>
-                <p className = "has-text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <h1 className = "title">About me</h1>
+                <p className = "has-text-left">I am a 1st Year computer science student at the University Of Warwick.</p>
             </div>
         )
     }

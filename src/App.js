ada78c372f2231c618b6677e64280496a1485596
hugo-wb <hugowong71@gmail.com>
2020-11-12 22:26:33 +0000

    new branch

--- src/App.js
+++ src/App.js
@@ -1,60 +1,58 @@
-import React, { Component } from 'react'
-import NavBar from "./Components/NavBar/NavBar"
-import {connect} from 'react-redux'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-  HashRouter,
-} from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
 
-import "./App.css"
-import "bulma/css/bulma.min.css"
-
-import Projects from "./Components/Projects/Projects"
-import About from "./Components/About/About"
-import Contact from "./Components/Contact/Contact"
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (
-      <Router basename = "/hugo-wb">
-      <div className = "background has-background-white-bis">
-        <div className = "mainCard">
-          <NavBar/>
-          <div className = "contentCard">
-          <Switch>
-            <Route path="/projects">
-              <Projects />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="/">
-              <Redirect exact from="/" to="/projects" />
-            </Route>
-          </Switch>
-          </div>
-        </div>
-      </div>
-      </Router>
-    )
-    
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => ({
-})
-const mapDispatchToProps = (dispatch) => ({
-
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App;
--- src/reducers/store.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import projectsReducer from "./projects/projectsReducer"
-import pageReducer from "./pageReducer"
-const allReducers = combineReducers({
-    projects: projectsReducer,
-    page:pageReducer,
-})
-
-export default allReducers;
